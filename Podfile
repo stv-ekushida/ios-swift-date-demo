6322f3baea9dfe89552863b403cb49db66583db7
e.kushida <e.kushida@st-ventures.jp>
2017-06-28 16:48:17 +0900

    ios-swift-date-demo

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ios-swift-date-demo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ios-swift-date-demo
+  pod 'SwiftDate', '~> 4.0'
+
+  target 'ios-swift-date-demoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ios-swift-date-demoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- ios-swift-date-demo/ViewController.swift
+++ ios-swift-date-demo/ViewController.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import SwiftDate
 
-class ViewController: UIViewController {
+
+/// https://github.com/malcommac/SwiftDate/tree/master/Documentation
+final class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("年",Date().year)
+        print("月",Date().month)
+        print("月あたりの日数",Date().monthDays, "日")
+        print("月", Date().monthName)
+        print("月(short)", Date().shortMonthName)
+        print("前月", Date().prevMonth.description)
+        print("翌月", Date().nextMonth.description)
+        print("何週目か？", Date().weekOfYear)
+        print("何曜日か？", Date().weekday)
+        print("何曜日か？", Date().weekdayName)
+        print("日", Date().day)
+        print("時間", Date().hour)
+        print("分", Date().minute)
+        print("秒", Date().second)
+        
+        let date1 = "2017-10-10 10:10".date(format: .custom("yyyy-MM-dd HH:MM"))
+        print(date1?.absoluteDate.description ?? "")
+        
+        let region = Region(tz: TimeZoneName.asiaTokyo,
+                            cal: CalendarName.gregorian,
+                            loc: LocaleName.japanese)
+        
+        let date2 = DateInRegion(string: "2017-10-10 10:10",
+                                 format: .custom("yyyy-MM-dd HH:MM"),
+                                 fromRegion: region)
+        print(date2?.absoluteDate.description ?? "")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
